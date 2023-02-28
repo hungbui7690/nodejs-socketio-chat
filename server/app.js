c14a2d9161fa7d3c0761d18dc9579a10655e7d1a
Hung Bui <hungbui7690@gmail.com>
2023-02-28 12:08:01 -0700

    Socket.io: Basic Express Server Setup

--- server/app.js
+++ server/app.js
@@ -1,4 +1,25 @@
-const path = require('path')
+/*
+  since we will upload to heroku, we need to tell heroku which command to run the app > package.json
+  - start
 
-console.log(__dirname + '/../public')
-console.log(path.join(__dirname, '/../public')) // normalize for us
+  we also need to tell heroku which version of nodejs that we use: 
+  - engines
+*/
+
+const path = require('path')
+const express = require('express')
+const app = express()
+
+const publicPath = path.join(__dirname, '/../public')
+
+//////////////////////////////////
+// MIDDLEWARE
+//////////////////////////////////
+
+app.use(express.static(publicPath))
+
+//////////////////////////////////
+// SERVER
+//////////////////////////////////
+const port = process.env.PORT || 3000
+app.listen(port, () => console.log(`Server is listening on port ${port}...`))
